a5a755943dc7bf41c8a0ef8d3623210cd9d5330e
ComputerSpieler <36859459+computerspieler@users.noreply.github.com>
2025-09-30 19:36:22 +0200

    Fix the key issue (No need to delete everything !)

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,9 +5,7 @@ build() {
 }
 
 install() {
-    adb uninstall fr.speilkoun.mangareader
-    adb shell 'rm /data/data/fr.speilkoun.mangareader/files/*'
-    adb install project/bin/MainActivity-debug.apk
+    adb install -r project/bin/MainActivity-debug.apk &&
     adb shell 'am start -n fr.speilkoun.mangareader/fr.speilkoun.mangareader.MainActivity'
 }
 
