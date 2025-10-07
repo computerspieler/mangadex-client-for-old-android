aebf70bb36a8be0cf4205f7d92fceffba2e7d014
ComputerSpieler <36859459+computerspieler@users.noreply.github.com>
2025-10-07 15:14:47 +0200

    Add refresh to the chapter activity

--- project/src/fr/speilkoun/mangareader/ChapterActivity.java
+++ project/src/fr/speilkoun/mangareader/ChapterActivity.java
@@ -2,18 +2,35 @@ package fr.speilkoun.mangareader;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
 import fr.speilkoun.mangareader.data.Database;
 import fr.speilkoun.mangareader.data.Serie;
+import fr.speilkoun.mangareader.sources.MangaDex;
+import fr.speilkoun.mangareader.utils.HTTPException;
 
 public class ChapterActivity extends Activity {
-    
+    static String TAG = "ChapterActivity";
     Serie mSerie;
 
 	void refreshList() {
 		ListView view = (ListView) this.findViewById(R.id.chapters_list);
 		view.setAdapter(Database.getInstance().adapterChapter(this, mSerie.id));
 	}
+
+    void refreshChapters() {
+        try {
+            MangaDex.loadChapters(mSerie.attribute);
+            this.refreshList();
+        } catch (HTTPException e) {
+            Toast.makeText(this, "Could not refresh", Toast.LENGTH_LONG)
+                .show();
+            Log.e(TAG, "Could not refresh: ", e);
+        }
+    }
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -25,5 +42,13 @@ public class ChapterActivity extends Activity {
 
 		this.setContentView(R.layout.chapters);
         this.refreshList();
+
+        Button button = (Button) this.findViewById(R.id.refresh);
+        button.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ChapterActivity.this.refreshChapters();
+            }
+        });
     }
 }
