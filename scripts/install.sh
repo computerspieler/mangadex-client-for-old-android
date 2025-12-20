77fb8cc009e52b89017de3cdb2582b2b37a11cd7
ComputerSpieler <36859459+computerspieler@users.noreply.github.com>
2025-12-20 22:41:01 +0100

    Install script: Improve its robustness

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,12 +1,35 @@
 #!/bin/bash
 
+PACKAGE=fr.speilkoun.mangareader
+
 build() {
     docker run -v .:/opt/ndk/apps/prj -i -t android-1.6 -c 'build prj debug'
 }
 
+install_apk() {
+    # We have to get grep's return code to check for a success because ADB
+    # Doesn't returns anything by default
+    adb install -r bin/MainActivity-debug.apk | tee /dev/stderr | grep -q Success
+}
+
 install() {
-    adb install -r bin/MainActivity-debug.apk &&
-    adb shell 'am start -n fr.speilkoun.mangareader/fr.speilkoun.mangareader.MainActivity'
+    install_apk
+    # In case of failure, try to fully reinstall the package
+    if [ $? -ne 0 ]; then
+        echo "Failed to reinstall the package, trying a full reinstallation"
+        adb uninstall "$PACKAGE"
+        install_apk
+    fi
+    if [ $? -eq 0 ]; then
+        # Kill the current process (if it exists, otherwise it will do nothing)
+        adb shell kill $(adb shell ps | grep -e 'fr.speilkoun.mangareader' | sed  's/[^ ]*[ ]*\([0-9]*\)[^0-9].*/\1/g')
+        # Start a new process
+        adb shell "am start -n $PACKAGE/$PACKAGE.MainActivity" & (
+            # We have to do this because am can get stuck
+            sleep 4; exit 0
+        )
+    fi
 }
 
 build && install
+exit $?
